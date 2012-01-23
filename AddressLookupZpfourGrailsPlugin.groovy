08bbf591b580eb4fc750dced7b9c8a66b761bba7
Aaron J. Zirbes <aaron.zirbes@gmail.com>
2012-01-23 00:41:50 -0600

    upgrading to 2.0.0 so docs look pretty

--- AddressLookupZpfourGrailsPlugin.groovy
+++ AddressLookupZpfourGrailsPlugin.groovy
@@ -14,29 +14,8 @@ class AddressLookupZpfourGrailsPlugin {
     def author = "Aaron J. Zirbes"
     def authorEmail = "ajz@umn.edu"
     def title = "Provides plugin-interface to Address Lookups via a Semaphore ZP4 HTTPSERV"
-    def description = '''\\
-Provides address lookup service and tags so you can easily add address lookups to your web app if you
-have a ZP4 HTTPSERV service from Semaphore corp running on your network.
-
-From their website:
-
-http://www.semaphorecorp.com/cgi/zp4.html
-
-ZP4 is a DVD-ROM containing United States addresses, ZIP + 4® codes, 
-mail carrier route numbers, and other supplemental postal databases 
-(all maintained by the Postal Service™), plus complete automatic 
-CASS Certified™ address correction software for Windows computers.
-
-This plugin assumes you already have a valid ZP4 DVD, and your Grails app 
-abides by the license agreements: Only accessed by your company, only installed
-on one computer, etc...
-
-For details, see: http://www.semaphorecorp.com/cgi/faq.html#21
-
-I, Aaron Zirbes, the author have no affiliation with Semaphore corp, other than that I am a 
-satisfied paying customer.  Their DVD is much more affortable than many similar products
-available on the market.
-'''
+    def description = '''Provides address lookup service and tags so you can easily 
+	add address lookups to your web app if you have a ZP4 HTTPSERV service from Semaphore corp running on your network.'''
 
     // URL to the plugin's documentation
     def documentation = "http://grails.org/plugin/address-lookup-zpfour"

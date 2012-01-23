83717e069305bc46bbb2b04511dde1f3a0e0800f
Aaron J. Zirbes <aaron.zirbes@gmail.com>
2012-01-23 00:31:53 -0600

    removing app content from gh-pages

--- grails-app/conf/Config.groovy
+++
@@ -1,9 +0,0 @@
-// The following properties have been added by the Upgrade process...
-grails.views.default.codec="none" // none, html, base64
-grails.views.gsp.encoding="UTF-8"
-
-grails.doc.authors = 'Aaron J. Zirbes'
-grails.doc.license = 'Apache License 2.0'
-
-// Added by the Address Lookup ZP4 Plugin:
-grails.plugins.addressLookupZpfour.server = 'http://zp4.intranet.example.com/'
